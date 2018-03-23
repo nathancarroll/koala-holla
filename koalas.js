334c58f6e7d6750dbda8bdbbf4f2e4233a170c66
Christopher Black <chris@primeacademy.io>
2018-03-22 21:18:10 -0500

    Giving the client side developers a head start.

--- koalas.js
+++ koalas.js
@@ -3,7 +3,7 @@
 	"name" : "Scotty",
 	"gender" : "M",
 	"age" : 4,
-  "ready_to_transer" : true,
+  "ready_to_transfer" : true,
   "notes" : "Born in Guatemala",
 	"__v" : 0
 }
@@ -12,7 +12,7 @@
 	"name" : "Jean",
 	"gender" : "F",
 	"age" : 5,
-  "ready_to_transer" : true,
+  "ready_to_transfer" : true,
   "notes" : "Allergic to lots of lava",
 	"__v" : 0
 }
@@ -21,7 +21,7 @@
 	"name" : "Ororo",
 	"gender" : "F",
 	"age" : 7,
-  "ready_to_transer" : false,
+  "ready_to_transfer" : false,
   "notes" : "Loves listening to Paula (Abdul)",
 	"__v" : 0
 }
@@ -30,7 +30,7 @@
 	"name" : "Logan",
 	"gender" : "M",
 	"age" : 15,
-  "ready_to_transer" : false,
+  "ready_to_transfer" : false,
   "notes" : "Loves the sauna",
 	"__v" : 0
 }
@@ -39,7 +39,7 @@
 	"name" : "Charlie",
 	"gender" : "M",
 	"age" : 9,
-  "ready_to_transer" : true,
+  "ready_to_transfer" : true,
   "notes" : "Favorite band is Nirvana",
 	"__v" : 0
 }
@@ -48,7 +48,7 @@
 	"name" : "Betsy",
 	"gender" : "F",
 	"age" : 4,
-  "ready_to_transer" : true,
+  "ready_to_transfer" : true,
   "notes" : "Has a pet iguana",
 	"__v" : 0
 }
--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Configure body-parser for Angular and jQuery
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // This line is required for Angular
 
 // Routes
+// Should these be in a router?
 
+// POST
+app.post('/koala', (req, res) => {
+  console.log('POST to /koala req.body =', req.body);
+});
+
+// GET
+app.get('/koala', (req, res) => {
+  // Temporary mock data. Replace this with mongoose.
+  const mockData = [{ _id: 1, name: 'Mock 1', gender: 'F', age: 5, ready_to_transfer: true, notes: 'n/a' },
+                    { _id: 2, name: 'Mock 2', gender: 'M', age: 5, ready_to_transfer: false, notes: 'n/a' }]
+  res.send(mockData);
+})
 
 // Static files
 app.use(express.static('server/public'));
 
 // Start listening for requests on a specific port
-app.listen(port, () => {
-  console.log('listening on port', port);
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
 });
