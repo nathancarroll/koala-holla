9ff2660337d577659485722a01ab33d1ed4cfc48
Roboronnie <leex4920@gmail.com>
2018-07-06 10:56:27 -0500

    app, controller in html set up, wrote a post method for addPet function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,24 @@
 console.log( 'js' );
+
+const app = angular.module('KoalaApp',[]);
+
+app.controller('KoalaController', ['$http', function($http) {
+    let self = this;
+
+    self.koalaList = [];
+
+    self.addKoala = function(koalaToAdd) {
+        $http({
+            url: '/koala',
+            method: 'POST',
+            data: koalaToAdd
+        }).then( function(response) {
+            console.log(response);
+        }).catch(function(err) {
+            console.log('error!', err);
+        });// end of POST method
+
+    }; // end of self.addKoala
+
+
+}]); // end of app.controller 
