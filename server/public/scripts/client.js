65ca9666cbd7663bb0546da2906832ae6f089344
Roboronnie <leex4920@gmail.com>
2018-07-06 11:11:08 -0500

    CRUD methods added

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,24 +1,60 @@
-console.log( 'js' );
+console.log('js');
 
-const app = angular.module('KoalaApp',[]);
+const app = angular.module('KoalaApp', []);
 
-app.controller('KoalaController', ['$http', function($http) {
+app.controller('KoalaController', ['$http', function ($http) {
     let self = this;
 
     self.koalaList = [];
 
-    self.addKoala = function(koalaToAdd) {
+    self.addKoala = function (koalaToAdd) {
         $http({
             url: '/koala',
             method: 'POST',
             data: koalaToAdd
-        }).then( function(response) {
+        }).then(function (response) {
             console.log(response);
-        }).catch(function(err) {
+            self.getKoalas();
+        }).catch(function (err) {
             console.log('error!', err);
         });// end of POST method
 
     }; // end of self.addKoala
 
+    self.getKoalas = function () {
 
+        $http({
+            url: '/koala',
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+            self.koalaList = response.data;
+        }).catch(function (err) {
+            console.log('Error message: ', err);
+        });
+    }// end of self.getKoalas
+
+    self.deleteKoala = function(id){
+        console.log('id of koala to remove', id);
+        $http({
+            url: `/koala/${id}`,
+            method: 'DELETE'
+        }).then(function(response){
+            console.log(response);
+            self.getKoalas();
+        }).catch(function(err){
+            console.log('Error: ', err);
+        });// end of delete method
+    } // end of self.deleteKoala
+
+    self.toggleReady = function(koala) {
+        console.log('toggle clicked');
+        $http({
+            url: `/koala/${koala._id}`,
+            method: 'PUT',
+            data: {readyToTransfer: true}
+        }).then(function(response){
+            console.log('Put Response', response);
+        }); // end of PUT method
+    }; // end of self.toggleReady
 }]); // end of app.controller 
